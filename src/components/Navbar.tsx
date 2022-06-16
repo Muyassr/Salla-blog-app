636de1e164d0dcb264fcaee89f0393bd9a4110ee
tamimm <tamimm@aecl.com>
2022-06-16 20:58:13 +0300

    hero section refinement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { MdOutlineDarkMode } from "react-icons/md";
 const Navbar = () => {
   return (
     <>
-      <div className="rtl bg-white flex p-5 px-14  justify-between align-baseline">
+      <div className="rtl bg-white flex p-5 px-52  justify-between align-baseline">
         <div className="flex items-center space-x-6 space-x-reverse">
           <a href="#" className="btn btn-ghost normal-case text-xl font-bold">
             Blog |مدونة
@@ -15,11 +15,11 @@ const Navbar = () => {
             <a href="#" className="hover:text-emerald-500  hover:underline underline-offset-8 transition-all">المقالات</a>
           </div>
         </div>
-        <div className="flex space-x-4 space-x-reverse">
-          <button className="rounded-full p-2 bg-slate-100 border border-stone-200 hover:bg-slate-200 transition-all">
+        <div className="flex space-x-3 space-x-reverse">
+          <button className="rounded-full p-3 text-lg bg-slate-100 border border-stone-200 hover:bg-slate-200 transition-all">
             <BiSearchAlt2 />
           </button>
-          <button className="rounded-full p-2 bg-slate-100 border border-stone-200 hover:bg-slate-200 transition-all">
+          <button className="rounded-full p-3 text-lg bg-slate-100 border border-stone-200 hover:bg-slate-200 transition-all">
             <MdOutlineDarkMode />
           </button>
 
--- src/pages/HomePage/Hero.tsx
+++ src/pages/HomePage/Hero.tsx
@@ -1,18 +1,19 @@
 import Navbar from "../../components/Navbar";
+import styles from './styles.scss' 
 import backgroundImage from "../../assets/diverse-people-working-office1.png";
 const Hero = () => {
-  const imgUrl = "src/assets/diverse-people-working-office1.png";
+  const imgUrl = "/src/assets/diverse-people-working-office1.png";
   return (
     <>
       <Navbar />
       <section
-        className={`flex p-20 bg-no-repeat min-h-screen bg-black bg-opacity-50 bg-blend-overlay bg-[url('${imgUrl}')]`}
+        className={`h-[32.5rem] p-5 px-52 flex bg-cover bg-no-repeat bg-black bg-opacity-50 bg-blend-overlay bg-[url('${imgUrl}')]`}
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
         <div className="container flex justify-end">
-          <div className="flex flex-col justify-end text-right border-r-4 px-5 border-emerald-400 text-white mt-auto">
-            <h1 className="font-bold text-5xl space-y-4">مركز المدونة</h1>
-            <p>
+          <div className="text-right border-r-4 px-5 border-emerald-400 text-white mt-auto">
+            <h1 className="font-bold text-5xl">مركز المدونة</h1>
+            <p className="my-4 text-2xl">
               مركــز للتعليـــم ونشــر المعرفة عبر المقالات والدونات المتخصصة
               بموضوعات التجارة الالكترونى والاقتصاد والتقنية , ومايخص حياتنا
               كتجار , كل ماتريده عن التجارة الالكترونية والاقتصاد
